4757689d22ba2e7afabf1fefece9213122d27858
Kjell <the_underpro@gmx.net>
2019-12-07 20:37:07 +0100

    respawning

---
+++ FreezingGoatsProject/Assets/Scripts/SpawningScript.cs
@@ -0,0 +1,39 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class SpawningScript : MonoBehaviour
+{
+    public static SpawningScript instance;
+
+    public GameObject PlayerPrefab1; 
+    public GameObject PlayerPrefab2;
+    public GameObject PlayerPrefab3;
+    public GameObject PlayerPrefab4;
+
+
+    public GameObject spawnPosition;
+
+
+    private void Awake()
+    {
+        instance = this; 
+    }
+
+    public void reportDeath(string controllerSuffix)
+    {
+        int playerNum = int.Parse(controllerSuffix);
+        GameObject toInstantiate = PlayerPrefab1;
+        switch(playerNum)
+        { 
+            case 2: toInstantiate = PlayerPrefab2; break;
+            case 3: toInstantiate = PlayerPrefab3; break;
+            case 4: toInstantiate = PlayerPrefab4; break; 
+
+        }
+        Instantiate(toInstantiate, spawnPosition.transform.position, Quaternion.identity); 
+
+    }
+
+
+}
