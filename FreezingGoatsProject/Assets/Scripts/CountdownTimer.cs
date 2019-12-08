ac178ebaf471507868f072b5956f98f7cda46424
Kjell <the_underpro@gmx.net>
2019-12-08 07:00:17 +0100

    countdownTimer prefab

---
+++ FreezingGoatsProject/Assets/Scripts/CountdownTimer.cs
@@ -0,0 +1,19 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+using UnityEngine.UI;
+
+public class CountdownTimer : MonoBehaviour
+{
+    public Text countdownText;
+    public float timeLeft = 180f; 
+     
+
+    // Update is called once per frame
+    void Update()
+    {
+        timeLeft -= Time.deltaTime;
+        int seconds = (((int)timeLeft) % 60);
+        countdownText.text = "" + ((int)(timeLeft / 60f)) + ":"  + ( (seconds<10) ? "0" : "" )+ seconds;
+    }
+}
